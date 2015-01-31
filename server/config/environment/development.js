210f1512f2d37777fb2ca6c3334ebcc0e05784af
Julian Mazzitelli <mazzitell.julian@gmail.com>
2015-01-31 05:12:49 -0500

    Set up db on dev and server

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,8 +5,9 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/syllascrape-dev'
+    //uri: 'mongodb://localhost/syllascrape-dev'
+    uri: 'mongodb://thejmazz:co34;lhh@ds063869.mongolab.com:63869/heroku_app33615486'
   },
 
-  seedDB: true
+  //seedDB: true
 };
