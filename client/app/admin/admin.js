7748cf05a73624e879129031f6daecd7f261965c
Julian Mazzitelli <mazzitell.julian@gmail.com>
2015-01-30 23:34:39 -0500

    angular-fullstack template

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('syllascrapeApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
